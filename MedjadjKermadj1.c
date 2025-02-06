7b0f2fbab2337ea835740c4a33a4b6d8e3d4da65
Zieir <165330036+Zieir@users.noreply.github.com>
2025-02-06 21:41:28 +0100

    Update MedjadjKermadj1.c

--- MedjadjKermadj1.c
+++ MedjadjKermadj1.c
@@ -1,11 +1,3 @@
-/*****************************{      RENDU PARTIE1      }******************************/
-/**                                                                                  **/
-/**   1. MEDJADJ Mohamed Abderraouf:   mohamed-abderraouf.medjadj@etu-upsaclay.fr    **/
-/**                                                                                  **/
-/**   2. KERMADJ Zineddine:   zineddine.kermadj@etu-upsaclay.fr                      **/
-/**                                                                                  **/
-/**************************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 
